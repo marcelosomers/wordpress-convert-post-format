e0726f32f4fba9b3a4cd448a7ca42074acab42fa
Marcelo Somers <msomers@gmail.com>
2014-01-06 13:29:52 -0600

    Initial Commit

---
+++ convert-to-linkpost.php
@@ -0,0 +1,30 @@
+<?php
+/**
+* Plugin Name: Convert Link Posts
+* Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+* Description: Converts a post to Link format when there is a link URL custom field
+* Version: 1.0
+* Author: Marcelo Somers
+* Author URI: http://marcelosomers.com/
+* License: MIT
+*/
+
+function set_link_post( $post_id ) {
+
+    // Set your linked list custom field name
+    $linked_list_field = "linked_list_url";
+
+    // Get the post format
+    $post_format = get_post_format( $post_id );
+
+    // Get the Linked List URL
+    $link_list_item = get_post_meta($post_id, 'linked_list_url', true);
+
+    if($post_format == FALSE && $link_list_item != "") {
+        set_post_format($post_id, 'Link');
+    }
+}
+
+add_action('save_post', 'set_link_post');
+
+?>
